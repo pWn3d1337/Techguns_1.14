659efce5068590dc86bddc2ad9f31ebf2a013bad
KingOfTheGamesYT <53309562+KingOfTheGamesYT@users.noreply.github.com>
2024-12-06 14:13:38 -0500

    removed these for now
    to make porting easier

--- src/main/java/techguns/api/capabilities/ITGShooterValues.java
+++
@@ -1,8 +0,0 @@
-package techguns.api.capabilities;
-
-public interface ITGShooterValues {
-
-	public AttackTime getAttackTime(boolean offHand);
-	public boolean isRecoiling(boolean offHand);
-	public boolean isReloading(boolean offHand);
-}
